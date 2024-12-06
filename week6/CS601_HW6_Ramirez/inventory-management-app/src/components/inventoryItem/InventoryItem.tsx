bde36c0557b2e2849ad124dfc1f22bfe3669ac34
Pedro Ramirez <prlara2000@gmail.com>
2024-12-06 00:08:36 -0600

    feat: add style

--- week6/CS601_HW6_Ramirez/inventory-management-app/src/components/inventoryItem/InventoryItem.tsx
+++ week6/CS601_HW6_Ramirez/inventory-management-app/src/components/inventoryItem/InventoryItem.tsx
@@ -4,22 +4,20 @@ import './InventoryItem.css';
 
 const InventoryItem = ({ item }: { item: InventoryItemType }) => {
   return (
-    <Link to={`/product/${item.SKU}`} state={{ item }}>
-      <div className='card'>
-        <h3 className='card-title'>{item.name}</h3>
-        <div className='card-content'>
-          <p>
-            <strong>SKU:</strong>
-            {item.SKU}
-          </p>
-          <p>
-            <strong>Price:</strong> {item.price}
-          </p>
-          <p>
-            <strong>In stock:</strong>
-            {item.qty}
-          </p>
-        </div>
+    <Link to={`/product/${item.SKU}`} state={{ item }} className='card'>
+      <h3 className='card-title'>{item.name}</h3>
+      <div className='card-content'>
+        <p>
+          <strong>SKU:</strong>
+          {item.SKU}
+        </p>
+        <p>
+          <strong>Price:</strong> {item.price}
+        </p>
+        <p>
+          <strong>In stock:</strong>
+          {item.qty}
+        </p>
       </div>
     </Link>
   );
--- week6/CS601_HW6_Ramirez/inventory-management-app/src/components/productDetail/ProductDetail.tsx
+++ week6/CS601_HW6_Ramirez/inventory-management-app/src/components/productDetail/ProductDetail.tsx
@@ -1,5 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { InventoryItemType } from '../../vite-env';
+import { Link } from 'react-router-dom';
+import './ProductDetail.css';
 
 const ProductDetail = () => {
   const location = useLocation();
@@ -10,22 +12,27 @@ const ProductDetail = () => {
   }
 
   return (
-    <div>
-      <h2>{item.name}</h2>
-      <p>
-        <strong>SKU:</strong> {item.SKU}
-      </p>
-      <p>
-        <strong>Price:</strong> {item.price}
-      </p>
-      <p>
-        <strong>In Stock:</strong> {item.qty}
-      </p>
-      <p>
-        <strong>Description:</strong> {item.description}
-      </p>
-      <img src={item.image} alt={item.name} />
-    </div>
+    <main className='pdp'>
+      <Link to='/' className='home-button'>
+        Home
+      </Link>
+      <div className='product-detail'>
+        <h2>{item.name}</h2>
+        <p>
+          <strong>SKU:</strong> {item.SKU}
+        </p>
+        <p>
+          <strong>Price:</strong> {item.price}
+        </p>
+        <p>
+          <strong>In Stock:</strong> {item.qty}
+        </p>
+        <p>
+          <strong>Description:</strong> {item.description}
+        </p>
+        <img className='pd-image' src={item.image} alt={item.name} />
+      </div>
+    </main>
   );
 };
 
